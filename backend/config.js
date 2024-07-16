a562a6a5f445384762c19ee124b5990708344960
Nipun Tiwari <happynipun2@gmail.com>
2024-07-17 00:22:10 +0530

    1st upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5556;
+
+export const mongoDBURL =
+  'mongodb+srv://root:root@book-store-mern.ymixsia.mongodb.net/?retryWrites=true&w=majority&appName=Book-store-MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
